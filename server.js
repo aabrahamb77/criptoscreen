37ca5d15ffebaed433e0eecbcb9db6cc2b4230df
aabrahamb77 <badachi_93@hotmail.com>
2026-05-21 19:46:51 -0700

    usar api.bytick.com para evitar bloqueo CloudFront

--- server.js
+++ server.js
@@ -8,7 +8,7 @@ app.use(express.static(path.join(__dirname, 'public')));
 function bybitGet(endpoint) {
   return new Promise((resolve, reject) => {
     const req = https.get(
-      `https://api.bybit.com${endpoint}`,
+      `https://api.bytick.com${endpoint}`,
       { headers: {
           'User-Agent': 'Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/125.0.0.0 Safari/537.36',
           'Accept': 'application/json, text/plain, */*',
